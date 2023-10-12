b21f1582aaab8819e44ddd7eab058a7c06110299
Daniel Kinyua <my.github.kinyuad29@gmail.com>
2023-10-12 17:57:49 +0300

    removinh header image

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const apiUrl = '"https://zenquotes.io/api/quotes/';
+
 
 // Function to fetch and display quotes
 function fetchAndDisplayQuotes() {
-    fetch(apiUrl)
+    const url = 'https://type.fit/api/quotes';
+    
+     fetch(url)
         .then((response) => {
+            console.log(response)
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
@@ -33,3 +36,29 @@ function displayQuotes(quotes) {
 
 // Call the function to fetch and display quotes
 fetchAndDisplayQuotes();
+// Event listener for the search form
+document.querySelector('#search-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+    const searchQuery = document.querySelector('#search-input').value;
+    // Implement search logic to filter quotes based on searchQuery
+});
+// Event listener for comment submission
+document.querySelector('#comment-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const commentText = document.querySelector('#comment-input').value;
+    // Handle comment submission and update the comment section
+});
+// Event listener for the like button
+document.querySelector('#like-button').addEventListener('click', function() {
+    // Handle the like action and update the UI
+});
+
+// Event listener for the dislike button
+document.querySelector('#dislike-button').addEventListener('click', function() {
+    // Handle the dislike action and update the UI
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code to fetch quotes, set up event listeners, and populate the page
+});
+
+
